b027906a4f9b8aeb35ef14e3fc7307d5029d4bc6
salemsifeddine <saiftony5@gmail.com>
2022-04-10 02:08:09 +0100

    main

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CKEDITOR_CONFIGS = {
     'default': {
